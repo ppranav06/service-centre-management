20e72cb2eb79d120f733cec2055faf92204e0760
aminroopkhs <poornimakkhs@gmail.com>
2024-06-03 14:38:08 +0530

    added more details and colors

--- Home_page.py
+++ Home_page.py
@@ -2,7 +2,7 @@ import tkinter as tk
 root=tk.Tk()
 root.geometry("1000x500")
 root.title("Vehicle Service Centre")
-options_frame=tk.Frame(root,bg="#c3c3c3")
+options_frame=tk.Frame(root,bg="#A89DC7")
 
 def job_page():
     job_frame=tk.Frame(main_frame)
@@ -16,39 +16,55 @@ def cus_page():
     lb.pack()
     cus_frame.pack(pady=20)
 
-def employe_page():
-    pass
+def assign_page():
+    assign_frame=tk.Frame(main_frame)
+    lb=tk.Label(assign_frame,text="ASSIGNED",font=("Bold",30))
+    lb.pack()
+    assign_frame.pack(pady=20)
+
+def unassign_page():
+    unassign_frame=tk.Frame(main_frame)
+    lb=tk.Label(unassign_frame,text="UNASSIGNED",font=("Bold",30))
+    lb.pack()
+    unassign_frame.pack(pady=20)
 
 def hide_indicators():
     job_indicate.config(bg="#c3c3c3")
     cus_indicate.config(bg="#c3c3c3")
-    other_indicate.config(bg="#c3c3c3")
+    assign_indicate.config(bg="#c3c3c3")
+    unassigned_indicate.config(bg="#c3c3c3")
 def delete_pages():
     for frame in main_frame.winfo_children():
         frame.destroy()
 def indicate(lb,page):
     hide_indicators()
-    lb.config(bg="#158aff")
+    lb.config(bg="#0B0707")
     delete_pages()
     page()
 
-job_btn=tk.Button(options_frame,text="Job Card",font=("Bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(job_indicate,job_page))
-job_btn.place(x=10,y=50)
+job_btn=tk.Button(options_frame,text="Job Card",font=("Bold",15),fg="#000000",bd=0,bg="#c3c3c3",command=lambda: indicate(job_indicate,job_page))
+job_btn.place(x=10,y=350)
 
 job_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
-job_indicate.place(x=3,y=50,width=5,height=40)
+job_indicate.place(x=3,y=350,width=5,height=40)
 
-cus_btn=tk.Button(options_frame,text="CustomerCard",font=("Bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(cus_indicate,cus_page))
-cus_btn.place(x=10,y=100)
+cus_btn=tk.Button(options_frame,text="CustomerCard",font=("Bold",15),fg="#000000",bd=0,bg="#c3c3c3",command=lambda: indicate(cus_indicate,cus_page))
+cus_btn.place(x=10,y=400)
 
 cus_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
-cus_indicate.place(x=3,y=100,width=5,height=40)
+cus_indicate.place(x=3,y=400,width=5,height=40)
 
-other_btn=tk.Button(options_frame,text="other",font=("Bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(other_indicate))
-other_btn.place(x=10,y=150)
+assigned_btn=tk.Button(options_frame,text="Assigned",font=("Bold",15),fg="#000000",bd=0,bg="#c3c3c3",command=lambda: indicate(assign_indicate,assign_page))
+assigned_btn.place(x=10,y=50)
 
-other_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
-other_indicate.place(x=3,y=150,width=5,height=40)
+assign_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
+assign_indicate.place(x=3,y=50,width=5,height=40)
+
+unassigned_btn=tk.Button(options_frame,text="Unassigned",font=("Bold",15),fg="#000000",bd=0,bg="#c3c3c3",command=lambda:indicate(unassigned_indicate,unassign_page))
+unassigned_btn.place(x=10,y=100)
+
+unassigned_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
+unassigned_indicate.place(x=3,y=100,width=5,height=40)
 
 options_frame.pack(side=tk.LEFT)
 options_frame.pack_propagate(False)
